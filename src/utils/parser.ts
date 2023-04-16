8f75841fb1ba8a7d0aed89c3802d98326d3aa864
CTN <info@ctn-originals.com>
2023-04-16 18:10:59 +0200

    Update parser.ts
    started to reform the parser

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -17,6 +17,68 @@ const DefaultCollectionToStringOptions: ICollectionToStringOptions = {
 	autoColor: true, //TODO
 }
 
+// class MessageObject {
+// 	/** 
+// 	 * @param {MessageObject} obj
+// 	*/
+// 	constructor(obj = {}) {
+// 		this.id = getMsgID();
+// 		this.tag = obj.tag ?? 'div';
+// 		this.type = obj.type ?? 'string';
+// 		this.content = obj.content ?? '';
+
+// 		this.depth = obj.depth ?? 0;
+// 		this.parent = obj.parent ?? null;
+		
+// 		this.children = [];
+// 		this.classNodes = [];
+// 		this.style = {};
+
+// 		this.html = '';
+// 		this.element = '';
+// 	}
+// }
+
+
+
+class MessageObject {
+	public Tag: string;
+	public Type: string;
+	public Indent: number;
+	public IndentString: string;
+
+	public Content: string;
+	public Depth: number;
+
+	public Parent: MessageObject | null;
+	public Children: MessageObject[];
+
+	public Color: string;
+	public BackgroundColor: string;
+	public Style: string;
+
+	/**
+	 * @param {MessageObject} obj
+	*/
+	constructor(obj: Partial<MessageObject> = {}) {
+		this.Tag = obj.Tag ?? 'value';
+		this.Type = obj.Type ?? 'string';
+		this.Indent = obj.Indent ?? 2;
+		this.IndentString = obj.IndentString ?? ' ';
+		
+		this.Content = obj.Content ?? '';
+		this.Depth = obj.Depth ?? 0;
+
+		this.Parent = obj.Parent ?? null;
+		this.Children = [];
+
+		this.Color = obj.Color ?? '';
+		this.BackgroundColor = obj.BackgroundColor ?? '';
+		this.Style = obj.Style ?? '';
+	}
+}
+
+
 /** 
  * @param {Object} input Input object to be converted to string
  * @param {ICollectionToStringOptions} options Options for the conversion
@@ -34,11 +96,66 @@ export function collectionToString(input: any, options: Partial<ICollectionToStr
 	//* and all its properties are defined
 	const safeOptions: Required<ICollectionToStringOptions> = mergedOptions as Required<ICollectionToStringOptions>;
 
-	const outputLines: string[] = formatCollectionToArray(input, safeOptions);
-	const output: string = applyIndentation(outputLines, safeOptions);
+	const msgObject: MessageObject = collectionToMessageObject(input, safeOptions);
+	// const outputLines: string[] = formatCollectionToArray(input, safeOptions);
+	// const output: string = applyIndentation(outputLines, safeOptions);
 
-	return output;
+	for (const child of msgObject.Children) {
+		console.log(child);
+		const childKeys = Object.keys(child);
+		const childValues = Object.values(child);
+		for (let i = 0 ; i < childKeys.length ; i++) {
+			console.log(`${childKeys[i]}: ${childValues[i]}`);
+			// for (const child2 of child.Children) {
+			// 	console.log(child2);
+			// 	const childKeys = Object.keys(child2);
+			// 	const childValues = Object.values(child2);
+			// 	for (let i = 0 ; i < childKeys.length ; i++) {
+			// 		console.log(`${childKeys[i]}: ${childValues[i]}`);
+			// 	}
+			// }
+		}
+	}
+
+	return '';
 }
+
+
+function collectionToMessageObject(collection: any, options: Required<ICollectionToStringOptions>, parent: MessageObject | null = null): MessageObject {
+	const msgObject: MessageObject = new MessageObject({
+		Indent: options.indent,
+		IndentString: options.indentString,
+		Depth: (parent) ? parent.Depth + 1 : 0,
+		Parent: parent,
+	});
+
+	for (const key in collection) {
+		const value = collection[key];
+
+		if (typeof value === 'object') {
+			const child = collectionToMessageObject(value, options, msgObject);
+			msgObject.Children.push(child);
+		}
+		else {
+			const child = new MessageObject({
+				Type: typeof value,
+				Indent: options.indent,
+				IndentString: options.indentString,
+				Depth: msgObject.Depth + 1,
+				Content: value,
+				Parent: msgObject,
+			});
+			msgObject.Children.push(child);
+		}
+	}
+
+	return msgObject;
+}
+
+
+
+
+
 
 const specialChar: string = '◘';
 const objBrackets: [string, string] = [`{${specialChar}`, `${specialChar}}`]; //* ◘ is a placeholder for the indentation to avoid the indentation being applied the user log message
