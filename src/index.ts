d128cf4df0aa70c5f33989b3feff5177cdaa3bc7
CTN <info@ctn-originals.com>
2023-06-13 15:53:12 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,9 @@ exports = {
 
 const cons = new handler.betterConsole.ConsoleInstance('');
 
+//* Wait 10 minutes before exiting
+setTimeout(() => {}, 1000 * 60 * 10); // 10 minutes
+
 // console.log('hello world');
 // console.log('hello world2');
 // console.log('x = [fg=red]' + cons.settings.indent + '[bg=white] y = ' + cons.settings.indentString);
