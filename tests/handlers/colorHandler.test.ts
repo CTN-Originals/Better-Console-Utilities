f602ff374786598103fa42fe106a8c971db2f17a
CTN <info@ctn-originals.com>
2023-07-06 15:46:07 +0200

    Update colorHandler.test.ts

--- tests/handlers/colorHandler.test.ts
+++ tests/handlers/colorHandler.test.ts
@@ -1,50 +1,116 @@
-import * as colorHandler from '../../src/handlers/colorHandler';
+import {
+	Color,
+	Theme,
+	colors,
+	styles,
+	getColor,
+} from '../../src/handlers/colorHandler';
 
 describe('ColorHandler', () => {
-
 	describe('Get Color', () => {
-		it('color name should return a valid color', () => {
-			const color: colorHandler.IColor = colorHandler.getColor('white');
+		it('hex color should return a valid color', () => {
+			const color: Color = getColor('#FFFFFF');
 			expect(color).toEqual({R: 255, G: 255, B: 255});
 		});
-		it('hex color should return a valid color', () => {
-			const color: colorHandler.IColor = colorHandler.getColor('#FFFFFF');
+		it('color name should return a valid color', () => {
+			const color: Color = getColor('white');
 			expect(color).toEqual({R: 255, G: 255, B: 255});
 		});
 
 		it('invalid input should return black', () => {
-			const color: colorHandler.IColor = colorHandler.getColor('invalid color');
-			expect(color).toEqual({R: 0, G: 0, B: 0});
+			const color: Color = getColor('invalid color');
+			expect(color).toEqual(colors.transparent);
 		});
 
 		it('should return the same color for the same input', () => {
-			const color1: colorHandler.IColor = colorHandler.getColor('white');
-			const color2: colorHandler.IColor = colorHandler.getColor('#FFFFFF');
+			const color1: Color = getColor('white');
+			const color2: Color = getColor('#FFFFFF');
 			expect(color1).toEqual(color2);
 		});
 	});
 
-	describe('Theme class', () => {
-		it('should create a theme with default values', () => {
-			const theme = new colorHandler.Theme();
-			expect(theme.foreground).toEqual(colorHandler.getColor('white'));
-			expect(theme.background).toEqual(null);
-			expect(theme.style).toEqual([]);
+	describe('Theme', () => {
+		describe('constructor', () => {
+			it('should create a theme with default values', () => {
+				const theme = new Theme();
+				expect(theme.foreground).toEqual(getColor('transparent'));
+				expect(theme.background).toEqual(getColor('transparent'));
+				expect(theme.style).toEqual([]);
+			});
+			
+			it('should create a theme with custom values', () => {
+				const theme = new Theme('red', 'white', 'bold');
+				expect(theme.foreground).toEqual(getColor('red'));
+				expect(theme.background).toEqual(getColor('white'));
+				expect(theme.style).toEqual([styles.bold]);
+			});
+			
+			it('should create a theme with custom values (array)', () => {
+				const theme = new Theme('red', 'white', ['bold', 'underscore']);
+				expect(theme.foreground).toEqual(getColor('red'));
+				expect(theme.background).toEqual(getColor('white'));
+				expect(theme.style).toEqual([styles.bold, styles.underscore]);
+			});
+			
+			it('should create a theme with custom values (null)', () => {
+				const theme = new Theme(null, null, null);
+				expect(theme.foreground).toEqual(getColor('transparent'));
+				expect(theme.background).toEqual(getColor('transparent'));
+				expect(theme.style).toEqual([]);
+			});
 		});
-
-		it('should create a theme with custom values', () => {
-			const theme = new colorHandler.Theme('red', 'white', 'bold');
-			expect(theme.foreground).toEqual(colorHandler.getColor('red'));
-			expect(theme.background).toEqual(colorHandler.getColor('white'));
-			expect(theme.style).toEqual([colorHandler.styles.bold]);
+		
+		describe('addStyle', () => {
+			it('should add a style to the style array', () => {
+				const theme = new Theme();
+				theme.addStyle('bold');
+				expect(theme.style).toEqual([styles.bold]);
+			});
+			
+			it('should add multiple styles to the style array', () => {
+				const theme = new Theme();
+				theme.addStyle('bold', 'underscore');
+				expect(theme.style).toEqual([styles.bold, styles.underscore]);
+			});
 		});
-
-		it('should create a theme with custom values (array)', () => {
-			const theme = new colorHandler.Theme('red', 'white', ['bold', 'underscore']);
-			expect(theme.foreground).toEqual(colorHandler.getColor('red'));
-			expect(theme.background).toEqual(colorHandler.getColor('white'));
-			expect(theme.style).toEqual([colorHandler.styles.bold, colorHandler.styles.underscore]);
+		
+		describe('removeStyle', () => {
+			it('should remove a style from the style array', () => {
+				const theme = new Theme(null, null, ['bold']);
+				theme.removeStyle('bold');
+				expect(theme.style).toEqual([]);
+			});
+			
+			it('should remove multiple styles from the style array', () => {
+				const theme = new Theme(null, null, ['bold', 'underscore']);
+				theme.removeStyle('bold', 'underscore');
+				expect(theme.style).toEqual([]);
+			});
+			
+			it('should not remove a style that is not in the style array', () => {
+				const theme = new Theme(null, null, ['bold']);
+				theme.removeStyle('italic');
+				expect(theme.style).toEqual([styles.bold]);
+			});
+		});
+		
+		describe('set style', () => {
+			it('should set the style array', () => {
+				const theme = new Theme();
+				theme.style = ['bold'];
+				expect(theme.style).toEqual([styles.bold]);
+			});
+			
+			it('should set the style array (string)', () => {
+				const theme = new Theme();
+				theme.style = 'bold';
+				expect(theme.style).toEqual([styles.bold]);
+			});
+			
+			it('should not set an invalid style', () => {
+				const theme = new Theme(null, null, ['invalid']);
+				expect(theme.style).toEqual([]);
+			});
 		});
 	});
-
 });
