5262ad43ed63e93862c0751d4427bfb892ced3f0
CTN <info@ctn-originals.com>
2023-11-20 15:59:03 +0100

    testing build
    Testing a few things and improving UX. This is by far not done yet.

--- src/handlers/consoleHandler.ts
+++ src/handlers/consoleHandler.ts
@@ -1,7 +1,7 @@
 import * as utils from '../utils';
-import * as color from './colorHandler';
+// import * as color from './colorHandler';
 
-interface IConsoleInstance {
+export interface IConsoleInstance {
 	name: string;
 	enabled: boolean;
 	suffix: string;
@@ -45,7 +45,7 @@ export class ConsoleInstance implements IConsoleInstance {
 	* @param {String} settings.indentString Indentation string of the console instance
 	* @param {Object} conditions Conditions for the console instance
 	*/
-	constructor(name: string = '', enabled: boolean = true, suffix: string = '', settings: {indent: number, indentString: string} = {indent: 2, indentString: '-'}, conditions: any = {}) {
+	constructor(name: string = '', enabled: boolean = true, suffix: string = '', settings: {indent: number, indentString: string} = {indent: 2, indentString: ' '}, conditions: any = {}) {
 		this.name = name;
 		this.enabled = enabled;
 		this.suffix = suffix;
